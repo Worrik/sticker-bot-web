8ff7c7a1bf606fd4a79fe3c448bc4a4271ad7a90
Worrik <vovachornand@gmail.com>
2024-05-11 23:14:15 +0300

    :rocket: update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,5 @@
-<script setup lang="ts">
-import { useTheme } from 'vuetify';
-
-const telergamTheme = computed(() => window?.Telegram?.WebApp?.themeParams);
-
-watch(
-  () => telergamTheme.value,
-  () => {
-    if (!telergamTheme.value) return;
-    const theme = useTheme();
-    theme.themes.value.telegramTheme = {
-      dark: window?.Telegram?.WebApp?.colorScheme === "dark",
-      colors: {
-        primary: telergamTheme.value.button_color || '',
-        background: telergamTheme.value.secondary_bg_color || '',
-        surface: telergamTheme.value.bg_color || '',
-        secondary: '',
-        success: '',
-        warning: '',
-        error: '',
-        info: '',
-        'on-background': telergamTheme.value.text_color || '',
-        'on-surface': telergamTheme.value.text_color || '',
-        'on-primary': telergamTheme.value.button_text_color || '',
-        'on-secondary': telergamTheme.value.text_color || '',
-        'on-success': '',
-        'on-warning': '',
-        'on-error': '',
-        'on-info': ''
-      },
-      variables: {},
-    }
-  },
-  { immediate: true }
-);
-</script>
 <template>
-  <v-theme-provider :theme="telergamTheme ? 'telegramTheme' : ''" :with-background="true">
+  <div>
     <slot />
-  </v-theme-provider>
+  </div>
 </template>
