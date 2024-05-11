84ff1a0f3ef8924de6366167b0e1b12fc91ae37b
Worrik <vovachornand@gmail.com>
2024-05-11 23:37:58 +0300

    :rocket: update

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -4,9 +4,30 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 
 export default defineNuxtPlugin((app) => {
-  console.log('Vuetify plugin', window?.Telegram)
+  const telegramTheme = window?.Telegram?.WebApp?.themeParams;
+  let customTelegramTheme = {};
+  if (telegramTheme) {
+    customTelegramTheme = {
+      dark: window?.Telegram?.WebApp?.colorScheme === 'dark',
+      colors: {
+        primary: telegramTheme.button_color || '',
+        background: telegramTheme.bg_color || '',
+        surface: telegramTheme.bg_color || '',
+        'on-background': telegramTheme.text_color || '',
+        'on-surface': telegramTheme.text_color || '',
+        'on-primary': telegramTheme.button_text_color || '',
+        'on-secondary': telegramTheme.text_color || '',
+      },
+      variables: {},
+    };
+  }
   const vuetify = createVuetify({
-    // ... your configuration
+    theme: {
+      defaultTheme: customTelegramTheme ? 'customTelegramTheme' : undefined,
+      themes: {
+        customTelegramTheme,
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
