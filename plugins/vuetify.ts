93354cc9d14b91657674ed7374539ff76b3b2c3a
Worrik <vovachornand@gmail.com>
2024-05-11 23:49:56 +0300

    :rocket: update

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -12,12 +12,13 @@ export default defineNuxtPlugin((app) => {
       colors: {
         primary: telegramTheme.button_color || '',
         background: telegramTheme.bg_color || '',
+        surface: telegramTheme.bg_color || '',
         'on-background': telegramTheme.text_color || '',
         'on-primary': telegramTheme.button_text_color || '',
       },
       variables: {},
     };
-    console.log('customTelegramTheme', customTelegramTheme);
+    console.log('customTelegramTheme', customTelegramTheme)
   }
   const vuetify = createVuetify({
     theme: {
