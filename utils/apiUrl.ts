55c11397dc6378685aec489d043a8d8264b1f2b7
Worrik <vovachornand@gmail.com>
2024-05-11 22:45:20 +0300

    :rocket: update

--- utils/apiUrl.ts
+++ utils/apiUrl.ts
@@ -2,8 +2,9 @@ export const apiUrl = process.env.NODE_ENV === 'development' ? 'http://localhost
 
 export function getWebAppInitData(): string {
   const initData = window.Telegram?.WebApp?.initData
-  if (initData && initData !== 'query_id') return initData;
-  const hash = window.location.hash;
-  if (!hash.includes('tgWebAppData=')) return '';
-  return hash.split('tgWebAppData=')[1].split('&tgWebAppVersion')[0];
+  if (initData && initData !== 'query_id') {
+    localStorage.setItem('tgWebAppData', initData);
+    return initData;
+  }
+  return localStorage.getItem('tgWebAppData') || '';
 }
