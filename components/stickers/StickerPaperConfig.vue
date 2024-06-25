f6f2b6a97ee7c369e8581395055cbcce80d9c2b4
Worrik <vovachornand@gmail.com>
2024-06-25 23:18:16 +0300

    :rocket: update

--- components/stickers/StickerPaperConfig.vue
+++ components/stickers/StickerPaperConfig.vue
@@ -90,7 +90,7 @@ function addOption() {
       >
         <v-icon>mdi-plus</v-icon>
       </v-btn>
-      <v-card variant="tonal" class="pa-1 mt-2 d-flex justify-end">
+      <v-card variant="text" class="pa-1 mt-2 d-flex justify-end">
         <v-card-title>
           <span v-for="(option, index) in options" :key="option.paperType">
             {{ option.quantity }} x {{ PAPER_COSTS[option.paperType] }}
