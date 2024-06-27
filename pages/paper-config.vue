beb739e80650f7d1cbb9398cdfd7398c83e622cc
Worrik <vovachornand@gmail.com>
2024-06-27 15:44:13 +0300

    :rocket: update

--- pages/paper-config.vue
+++ pages/paper-config.vue
@@ -44,16 +44,31 @@ async function createOrder() {
 </script>
 
 <template>
-  <div class="pa-4">
-    <div class="d-flex flex-column ga-4">
-      <StickersStickerPaperConfig
-        v-for="stickerItem in cart"
-        :key="stickerItem.sticker.id"
-        v-model:sticker="stickerItem.sticker"
-        :options="stickerItem.options"
+  <div>
+    <v-app-bar>
+      <v-app-bar-title>Обери стікери</v-app-bar-title>
+      <template #append>
+        <v-chip :color="orderSumPrice > 0 ? 'primary' : 'grey'" text-color="white" class="mr-4">
+          <v-icon left>mdi-sticker</v-icon>
+          ₴ {{ orderSumPrice }}
+        </v-chip>
+      </template>
+    </v-app-bar>
+    <div class="pa-4">
+      <div class="d-flex flex-column ga-4">
+        <StickersStickerPaperConfig
+          v-for="stickerItem in cart"
+          :key="stickerItem.sticker.id"
+          v-model:sticker="stickerItem.sticker"
+          :options="stickerItem.options"
+        />
+      </div>
+      <tg-back-button @click="router.push('/stickers')" />
+      <tg-main-button
+        v-if="orderSumPrice > 0"
+        :text="`Підтвердити замовлення (${orderSumPrice} грн.)`"
+        @click="createOrder"
       />
     </div>
-    <tg-back-button @click="router.push('/stickers')" />
-    <tg-main-button :text="`Підтвердити замовлення (${orderSumPrice} грн.)`" @click="createOrder" />
   </div>
 </template>
