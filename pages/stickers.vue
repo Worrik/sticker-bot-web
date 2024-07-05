0c0f4fb118656eed7b64ced27eba4c6a77d32b26
Volodymyr <vovachornand@gmail.com>
2024-07-05 20:08:56 +0300

    :rocket: update

--- pages/stickers.vue
+++ pages/stickers.vue
@@ -21,6 +21,9 @@ const stickersDataProvider = useGenericPaginatedDataProvider<ISticker>({
 
 const cart = useState<Array<IStickerCartItem>>('cart', () => []);
 
+const progress = ref(0);
+const loading = ref(false);
+
 const selectedStickers = computed<Array<ISticker>>(() => {
   return cart.value.map((item) => item.sticker);
 });
@@ -73,14 +76,15 @@ async function removeStickers(stickers: Array<ISticker>) {
     async (ok: boolean) => {
       if (!ok) return;
       cart.value = cart.value.filter((item) => !stickers.includes(item.sticker));
-      for (const sticker of stickers) {
+      stickers.forEach(async (sticker, index) => {
+        progress.value = Math.round(((index + 1) / stickers.length) * 100);
         await $fetch(`${apiUrl}/stickers/${sticker.id}/`, {
           method: 'DELETE',
           headers: {
             Authorization: getWebAppInitData(),
           },
         });
-      }
+      });
       stickersDataProvider.reset();
       await stickersDataProvider.loadMore();
     }
@@ -94,6 +98,13 @@ async function goToPaperConfig() {
 
 <template>
   <div>
+    <v-progress-linear
+      :active="loading"
+      :indeterminate="loading"
+      color="primary"
+      absolute
+      bottom
+    ></v-progress-linear>
     <v-app-bar>
       <v-app-bar-title>Обери стікери</v-app-bar-title>
       <template #append>
@@ -112,82 +123,87 @@ async function goToPaperConfig() {
         </v-btn>
       </template>
     </v-app-bar>
-    <div class="py-12"></div>
-    <div
-      class="px-4 pb-4 ga-4 d-flex flex-wrap"
-      v-if="stickersDataProvider.loading || stickersDataProvider.items.length"
-    >
-      <v-btn
-        variant="tonal"
-        color="primary"
-        appendIcon="mdi-check"
-        :disabled="stickersCount === stickersDataProvider.items.length"
-        @click="selectAll"
+    <v-main :class="{ 'opacity-50': loading, 'pointer-events-none': loading }">
+      <div
+        class="px-4 pb-4 ga-4 d-flex flex-wrap"
+        v-if="stickersDataProvider.loading || stickersDataProvider.items.length"
       >
-        Обрати всі
-      </v-btn>
-      <v-btn
-        variant="tonal"
-        color="primary"
-        appendIcon="mdi-trash-can"
-        :disabled="stickersCount === 0"
-        @click="removeStickers(selectedStickers)"
-      >
-        Видалити обрані
-      </v-btn>
-    </div>
-    <div class="d-flex flex-wrap align-center w-100 ga-4 pb-16 flex-column">
-      <v-container>
-        <v-alert
-          v-if="stickersDataProvider.error"
-          border="top"
-          type="error"
-          variant="outlined"
-          prominent
+        <v-btn
+          variant="tonal"
+          color="primary"
+          appendIcon="mdi-check"
+          :disabled="stickersCount === stickersDataProvider.items.length"
+          @click="selectAll"
         >
-          <template #title>Щось зламалось.. 😔</template>
-        </v-alert>
-        <v-alert
-          v-else-if="stickersDataProvider.items.length === 0 && !stickersDataProvider.loading && stickersDataProvider.paginator.page"
-          border="top"
-          type="info"
-          variant="outlined"
-          prominent
+          Обрати всі
+        </v-btn>
+        <v-btn
+          variant="tonal"
+          color="primary"
+          appendIcon="mdi-trash-can"
+          :disabled="stickersCount === 0"
+          @click="removeStickers(selectedStickers)"
         >
-          <template #title>У вас немає збережених стікерів.</template>
-          <template #text> Надсилайте стікери у чаті з ботом, щоб вони з'явилися тут. </template>
-        </v-alert>
-        <v-row v-else class="justify-center flex-wrap">
-          <div class="d-flex justify-end flex-column px-2 ga-4">
-            <StickersStickerSelect
-              v-for="sticker in oddCartColumn"
-              :key="sticker.id"
-              :sticker="sticker"
-              :selected="isStickerSelected(sticker)"
-              @addToCart="addToCart(sticker)"
-              @removeFromCart="removeFromCart(sticker)"
-            />
-          </div>
-          <div class="d-flex justify-start flex-column px-2 ga-4">
-            <StickersStickerSelect
-              v-for="sticker in evenCartColumn"
-              :key="sticker.id"
-              :sticker="sticker"
-              :selected="isStickerSelected(sticker)"
-              @addToCart="addToCart(sticker)"
-              @removeFromCart="removeFromCart(sticker)"
-            />
-          </div>
-        </v-row>
-      </v-container>
-      <v-infinite-scroll @load="load">
-        <template #empty>
-          <div class="pa-2"></div>
-        </template>
-        <template #error>
-          <div class="pa-2"></div>
-        </template>
-      </v-infinite-scroll>
-    </div>
+          Видалити обрані
+        </v-btn>
+      </div>
+      <div class="d-flex flex-wrap align-center w-100 ga-4 pb-16 flex-column">
+        <v-container>
+          <v-alert
+            v-if="stickersDataProvider.error"
+            border="top"
+            type="error"
+            variant="outlined"
+            prominent
+          >
+            <template #title>Щось зламалось.. 😔</template>
+          </v-alert>
+          <v-alert
+            v-else-if="
+              stickersDataProvider.items.length === 0 &&
+              !stickersDataProvider.loading &&
+              stickersDataProvider.paginator.page
+            "
+            border="top"
+            type="info"
+            variant="outlined"
+            prominent
+          >
+            <template #title>У вас немає збережених стікерів.</template>
+            <template #text> Надсилайте стікери у чаті з ботом, щоб вони з'явилися тут. </template>
+          </v-alert>
+          <v-row v-else class="justify-center flex-wrap">
+            <div class="d-flex justify-end flex-column px-2 ga-4">
+              <StickersStickerSelect
+                v-for="sticker in oddCartColumn"
+                :key="sticker.id"
+                :sticker="sticker"
+                :selected="isStickerSelected(sticker)"
+                @addToCart="addToCart(sticker)"
+                @removeFromCart="removeFromCart(sticker)"
+              />
+            </div>
+            <div class="d-flex justify-start flex-column px-2 ga-4">
+              <StickersStickerSelect
+                v-for="sticker in evenCartColumn"
+                :key="sticker.id"
+                :sticker="sticker"
+                :selected="isStickerSelected(sticker)"
+                @addToCart="addToCart(sticker)"
+                @removeFromCart="removeFromCart(sticker)"
+              />
+            </div>
+          </v-row>
+        </v-container>
+        <v-infinite-scroll @load="load">
+          <template #empty>
+            <div class="pa-2"></div>
+          </template>
+          <template #error>
+            <div class="pa-2"></div>
+          </template>
+        </v-infinite-scroll>
+      </div>
+    </v-main>
   </div>
 </template>
