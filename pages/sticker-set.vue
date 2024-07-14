cde2701ed1410b8c5d391f8a5eb5559474063008
Volodymyr <vovachornand@gmail.com>
2024-07-15 01:23:31 +0300

    :rocket: update

--- pages/sticker-set.vue
+++ pages/sticker-set.vue
@@ -87,7 +87,7 @@ async function addStickers() {
           absolute
           bottom
         ></v-progress-linear>
-        <v-row class="mt-6 mb-4 px-2">
+        <v-row class="my-2 px-4">
           <v-btn
             variant="tonal"
             color="primary"
